6161cd407a5c6155d1961579d6896367ae4d8a4d
json <jasonvibasarte@outlook.com>
2023-07-11 10:00:46 +0800

    initial commit

---
+++ Blognet/Areas/Identity/Data/BlognetDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Blognet.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blognet.Data;
+
+public class BlognetDbContext : IdentityDbContext<ApplicationUser>
+{
+    public BlognetDbContext(DbContextOptions<BlognetDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Blognet/Program.cs
+++ Blognet/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Blognet.Data;
+using Blognet.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BlognetDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BlognetDbContextConnection' not found.");
+
+builder.Services.AddDbContext<BlognetDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BlognetDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
@@ -19,5 +31,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
